5968eb86e983de5d1e2acd660bf00d02cb8365da
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-19 19:20:58 -0300

    Creating HomeActivity and setting its nav_graph

---
+++ features/home/src/main/java/cards/features/home/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package cards.features.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
