61555760e38fec50128dd8664ef6b0febf82701c
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-20 10:36:39 -0300

    Correcting classes to reflect repository changes

--- features/home/src/main/java/cards/features/home/view/HomeActivity.kt
+++ features/home/src/main/java/cards/features/home/view/HomeActivity.kt
@@ -3,8 +3,7 @@ package cards.features.home.view
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import cards.features.home.R
-import cards.features.home.di.HomeKoinModule
+import cards.features.home.di.module
 import cards.features.home.viewmodel.HomeViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.component.KoinApiExtension
@@ -20,18 +19,16 @@ class HomeActivity: AppCompatActivity(), KoinComponent {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.home_activity)
-        loadKoinModules(HomeKoinModule.homeModule())
+        loadKoinModules(module)
 
         homeViewModel.widgetsLiveData.observe(this, {
             Log.d("Test", "widgets")
         })
-
-        homeViewModel.getWidgets()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unloadKoinModules(HomeKoinModule.homeModule())
+        unloadKoinModules(module)
     }
 
 }
--- features/home/src/main/java/cards/features/home/viewmodel/HomeViewModel.kt
+++ features/home/src/main/java/cards/features/home/viewmodel/HomeViewModel.kt
@@ -1,23 +1,14 @@
 package cards.features.home.viewmodel
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import cards.features.home.model.WidgetList
-import cards.features.home.networking.HomeService
-import kotlinx.coroutines.launch
+import cards.features.home.networking.WidgetRepository
 
 class HomeViewModel(
-    private val service: HomeService
+    repository: WidgetRepository
 ): ViewModel() {
 
-    val widgetsLiveData = MutableLiveData<WidgetList>()
-
-    fun getWidgets() {
-        viewModelScope.launch {
-            val widgets = service.gethomeWidgets()
-            widgetsLiveData.value = widgets
-        }
-    }
+    var widgetsLiveData: LiveData<WidgetList> = repository.getWidgets()
 
 }
