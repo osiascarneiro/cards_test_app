ebfa1ffea67a26b6b45b9dbca32713cda56def4d
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-20 18:37:25 -0300

    Updating actions

--- actions/src/main/java/cards/actions/Actions.kt
+++ actions/src/main/java/cards/actions/Actions.kt
@@ -9,8 +9,8 @@ object Actions {
     const val ACCOUNT_ID_EXTRA = "ACCOUNT_ID_EXTRA"
 
     fun openHomeIntent(context: Context) = internalIntent(context, "com.osias.cards.home.open")
-    fun openCardDetails(context: Context, cardId: String) = internalIntent(context, "com.osias.cards.home.open").putExtra(CARD_ID_EXTRA, cardId)
-    fun openAccountDetails(context: Context, accountId: String) = internalIntent(context, "com.osias.cards.home.open").putExtra(ACCOUNT_ID_EXTRA, accountId)
+    fun openCardDetails(context: Context, cardId: String) = internalIntent(context, "com.osias.cards.cardDetails.open").putExtra(CARD_ID_EXTRA, cardId)
+    fun openAccountDetails(context: Context, accountId: String) = internalIntent(context, "com.osias.cards.accountDetails.open").putExtra(ACCOUNT_ID_EXTRA, accountId)
 
     fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
 }
