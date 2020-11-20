58dfc66acc5733563a86b0403b81001652187d35
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-20 10:35:39 -0300

    Reorganizing koin module

--- features/home/src/main/java/cards/features/home/di/HomeKoinModule.kt
+++ features/home/src/main/java/cards/features/home/di/HomeKoinModule.kt
@@ -1,33 +1,13 @@
 package cards.features.home.di
 
-import cards.features.home.networking.HomeService
+import cards.features.home.networking.WidgetRepository
+import cards.features.home.networking.WidgetRepositoryInterface
 import cards.features.home.viewmodel.HomeViewModel
 import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.core.module.Module
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-object HomeKoinModule {
-
-    @JvmStatic
-    fun getBaseUrl() = "https://mpay-android-interview.herokuapp.com/android/interview/"
-
-    @JvmStatic
-    fun homeModule(): Module {
-        return module {
-            single { buildRetrofitService() }
-            viewModel { HomeViewModel(get()) }
-        }
-    }
-
-    @JvmStatic
-    fun buildRetrofitService(): HomeService {
-        val retrofit = Retrofit.Builder()
-                            .baseUrl(getBaseUrl())
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .build()
-        return retrofit.create(HomeService::class.java)
-    }
-
+val module = module {
+    single { RetrofitBuilder.buildRetrofitService() }
+    factory { WidgetRepository(get()) }
+    viewModel { HomeViewModel(get()) }
 }
