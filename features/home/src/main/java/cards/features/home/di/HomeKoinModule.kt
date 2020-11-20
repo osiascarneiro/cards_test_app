61bbd5ea381376690c3820615b2c5cf9902ec409
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-20 11:04:48 -0300

    Binding and interface and not a concrete class

--- features/home/src/main/java/cards/features/home/di/HomeKoinModule.kt
+++ features/home/src/main/java/cards/features/home/di/HomeKoinModule.kt
@@ -4,10 +4,11 @@ import cards.features.home.networking.WidgetRepository
 import cards.features.home.networking.WidgetRepositoryInterface
 import cards.features.home.viewmodel.HomeViewModel
 import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.binds
 import org.koin.dsl.module
 
 val module = module {
     single { RetrofitBuilder.buildRetrofitService() }
-    factory { WidgetRepository(get()) }
+    factory { WidgetRepository(get()) } binds arrayOf(WidgetRepositoryInterface::class)
     viewModel { HomeViewModel(get()) }
 }
