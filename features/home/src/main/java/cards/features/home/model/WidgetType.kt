a3af7562a223f87e109f6381d49bba01ff19c4be
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-20 10:35:49 -0300

    Put back widget test

--- features/home/src/main/java/cards/features/home/model/WidgetType.kt
+++ features/home/src/main/java/cards/features/home/model/WidgetType.kt
@@ -3,8 +3,8 @@ package cards.features.home.model
 import com.google.gson.annotations.SerializedName
 
 enum class WidgetType {
-//    @SerializedName("HOME_HEADER_WIDGET")
-//    HOME_HEADER,
+    @SerializedName("HOME_HEADER_WIDGET")
+    HOME_HEADER,
     @SerializedName("HOME_CARD_WIDGET")
     HOME_CARD,
     @SerializedName("HOME_STATEMENT_WIDGET")
