63304191aee7f0a0317002ebbd0f1b772ba62484
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-23 19:21:10 -0300

    Adding custom matchers and verifying list count and title

--- features/home/src/androidTest/java/cards/features/home/HomeActivityTest.kt
+++ features/home/src/androidTest/java/cards/features/home/HomeActivityTest.kt
@@ -2,24 +2,23 @@ package cards.features.home
 
 import android.content.Intent
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import cards.features.home.di.mockModule
-import cards.features.home.di.module
+import cards.features.home.util.CustomMatchers.atPosition
+import cards.features.home.util.CustomMatchers.withItemCount
 import cards.features.home.view.HomeActivity
-import org.junit.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.GlobalContext
-import org.koin.core.context.loadKoinModules
-import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
@@ -49,8 +48,11 @@ class HomeActivityTest {
     }
 
     @Test
-    fun testWidgetList() {
+    fun testSuccessWithComplete3Itens() {
         onView(withId(R.id.loadingBar)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        onView(withId(R.id.errorText)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        onView(withId(R.id.widgetList)).check(matches(withItemCount(3)))
+        onView(withId(R.id.widgetList)).check(matches(atPosition(1, withText("mock title"))))
     }
 
 }
