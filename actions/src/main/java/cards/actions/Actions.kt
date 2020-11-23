688c9a5a31903399e09bb4417acd9728bd7fc09b
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-23 14:27:54 -0300

    Make internal intent private and removing placeholder classes

--- actions/src/main/java/cards/actions/Actions.kt
+++ actions/src/main/java/cards/actions/Actions.kt
@@ -12,5 +12,5 @@ object Actions {
     fun openCardDetails(context: Context, cardId: String) = internalIntent(context, "com.osias.cards.cardDetails.open").putExtra(CARD_ID_EXTRA, cardId)
     fun openAccountDetails(context: Context, accountId: String) = internalIntent(context, "com.osias.cards.accountDetails.open").putExtra(ACCOUNT_ID_EXTRA, accountId)
 
-    fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
+    private fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
 }
