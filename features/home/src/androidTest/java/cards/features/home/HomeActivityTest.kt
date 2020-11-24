20b13732ad9335ca04b16eed0513063bfb44b218
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-24 11:52:15 -0300

    Finishing HomeRobot

--- features/home/src/androidTest/java/cards/features/home/HomeActivityTest.kt
+++ features/home/src/androidTest/java/cards/features/home/HomeActivityTest.kt
@@ -1,29 +1,13 @@
 package cards.features.home
 
-import android.app.Instrumentation
 import android.content.Intent
-import android.content.IntentFilter
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
-import cards.actions.Actions
-import cards.features.home.util.CustomMatchers.atPosition
-import cards.features.home.util.CustomMatchers.withItemCount
 import cards.features.home.view.HomeActivity
-import cards.features.home.view.widgets.HomeAccountView
-import cards.features.home.view.widgets.HomeCardView
-import cards.features.home.view.widgets.HomeHeaderView
-import org.hamcrest.Matchers.instanceOf
-import org.hamcrest.core.AllOf.allOf
 import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -74,22 +58,20 @@ class HomeActivityTest {
 
     @Test
     fun testChangeToCardDetails() {
-        val filter = IntentFilter(Actions.CARD_DETAILS_ACTION)
-        val am = Instrumentation.ActivityMonitor(filter, null, true)
-        InstrumentationRegistry.getInstrumentation().addMonitor(am)
-        onView(allOf(withId(R.id.actionButton), withText("mock card details"))).perform(click())
-        assert(InstrumentationRegistry.getInstrumentation().checkMonitorHit(am, 1))
-        InstrumentationRegistry.getInstrumentation().removeMonitor(am)
+        home {
+            val am = addCardDetailsInstrumentationMonitor()
+            performClickInCardDetailsButton()
+            checkInstrumentationMonitorHitAndRemove(am)
+        }
     }
 
     @Test
     fun testChangeToAccountDetails() {
-        val filter = IntentFilter(Actions.ACCOUNT_DETAILS_ACTION)
-        val am = Instrumentation.ActivityMonitor(filter, null, true)
-        InstrumentationRegistry.getInstrumentation().addMonitor(am)
-        onView(allOf(withId(R.id.actionButton), withText("mock account details"))).perform(click())
-        assert(InstrumentationRegistry.getInstrumentation().checkMonitorHit(am, 1))
-        InstrumentationRegistry.getInstrumentation().removeMonitor(am)
+        home {
+            val am = addAccountDetailsInstrumentationMonitor()
+            performClickInAccountDetailsButton()
+            checkInstrumentationMonitorHitAndRemove(am)
+        }
     }
 
 }
--- features/home/src/androidTest/java/cards/features/home/HomeRobot.kt
+++ features/home/src/androidTest/java/cards/features/home/HomeRobot.kt
@@ -1,9 +1,14 @@
 package cards.features.home
 
+import android.app.Instrumentation
+import android.content.IntentFilter
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import cards.actions.Actions
 import cards.features.home.util.CustomMatchers
 import cards.features.home.view.widgets.HomeAccountView
 import cards.features.home.view.widgets.HomeCardView
@@ -16,23 +21,23 @@ fun home(func: HomeRobot.() -> Unit) = HomeRobot().apply { func() }
 class HomeRobot {
     //Reutilizable private methods
     private fun checkVisibilityWithId(id: Int, visibility: ViewMatchers.Visibility): ViewInteraction =
-        Espresso.onView(ViewMatchers.withId(id))
+        getViewWithId(id)
             .check(
                 ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(visibility))
             )
 
-    private fun checkNumberOfItensInList(count: Int): ViewInteraction = Espresso.onView(ViewMatchers.withId(R.id.widgetList))
+    private fun checkNumberOfItensInList(count: Int): ViewInteraction = getViewWithId(R.id.widgetList)
         .check(ViewAssertions.matches(CustomMatchers.withItemCount(count)))
 
     private fun checkInstaceTypeInPosition(position: Int, classType: Class<*>): ViewInteraction =
-        Espresso.onView(ViewMatchers.withId(R.id.widgetList))
+        getViewWithId(R.id.widgetList)
             .check(
                 ViewAssertions.matches(
                     CustomMatchers.atPosition(position, AllOf.allOf(Matchers.instanceOf(classType)))
                 )
             )
 
-    private fun checkTextInPosition(position: Int, text: String): ViewInteraction = Espresso.onView(ViewMatchers.withId(R.id.widgetList))
+    private fun checkTextInPosition(position: Int, text: String): ViewInteraction = getViewWithId(R.id.widgetList)
         .check(
             ViewAssertions.matches(
                 CustomMatchers.atPosition(
@@ -41,6 +46,14 @@ class HomeRobot {
                 )
             )
         )
+
+    private fun getViewWithId(id: Int) = Espresso.onView(ViewMatchers.withId(id))
+    private fun getViewWithIdAndTitle(id: Int, title: String) = Espresso.onView(
+        AllOf.allOf(
+            ViewMatchers.withId(id),
+            ViewMatchers.withText(title)
+        )
+    )
 
     fun checkLoadingGone() = checkVisibilityWithId(R.id.loadingBar, ViewMatchers.Visibility.GONE)
     fun checkErrorGone() = checkVisibilityWithId(R.id.errorText, ViewMatchers.Visibility.GONE)
@@ -60,4 +73,26 @@ class HomeRobot {
     fun checkSaldoInThirdPosition() = checkTextInPosition(2, "Saldo")
     fun checkValueInThirdPosition() = checkTextInPosition(2, "R$ 50,00")
     fun checkMockAccountDetailsInThirdPosition() = checkTextInPosition(2, "mock account details")
+
+    fun performClickInCardDetailsButton() = getViewWithIdAndTitle(R.id.actionButton, "mock card details").perform(click())
+    fun performClickInAccountDetailsButton() = getViewWithIdAndTitle(R.id.actionButton, "mock account details").perform(click())
+
+    fun addCardDetailsInstrumentationMonitor(): Instrumentation.ActivityMonitor {
+        val filter = IntentFilter(Actions.CARD_DETAILS_ACTION)
+        val am = Instrumentation.ActivityMonitor(filter, null, true)
+        InstrumentationRegistry.getInstrumentation().addMonitor(am)
+        return am
+    }
+
+    fun addAccountDetailsInstrumentationMonitor(): Instrumentation.ActivityMonitor {
+        val filter = IntentFilter(Actions.ACCOUNT_DETAILS_ACTION)
+        val am = Instrumentation.ActivityMonitor(filter, null, true)
+        InstrumentationRegistry.getInstrumentation().addMonitor(am)
+        return am
+    }
+
+    fun checkInstrumentationMonitorHitAndRemove(am: Instrumentation.ActivityMonitor) {
+        assert(InstrumentationRegistry.getInstrumentation().checkMonitorHit(am, 1))
+        InstrumentationRegistry.getInstrumentation().removeMonitor(am)
+    }
 }
