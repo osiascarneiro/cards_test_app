c759dfedd4dd0c671dd6aa7c60c7bd89b60b7deb
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-11-24 10:45:24 -0300

    Putting actions in consts to use in tests asserts

--- actions/src/main/java/cards/actions/Actions.kt
+++ actions/src/main/java/cards/actions/Actions.kt
@@ -8,9 +8,13 @@ object Actions {
     const val CARD_ID_EXTRA = "CARD_ID_EXTRA"
     const val ACCOUNT_ID_EXTRA = "ACCOUNT_ID_EXTRA"
 
-    fun openHomeIntent(context: Context) = internalIntent(context, "com.osias.cards.home.open")
-    fun openCardDetails(context: Context, cardId: String) = internalIntent(context, "com.osias.cards.cardDetails.open").putExtra(CARD_ID_EXTRA, cardId)
-    fun openAccountDetails(context: Context, accountId: String) = internalIntent(context, "com.osias.cards.accountDetails.open").putExtra(ACCOUNT_ID_EXTRA, accountId)
+    const val HOME_ACTION = "com.osias.cards.home.open"
+    const val CARD_DETAILS_ACTION = "com.osias.cards.cardDetails.open"
+    const val ACCOUNT_DETAILS_ACTION = "com.osias.cards.accountDetails.open"
+
+    fun openHomeIntent(context: Context) = internalIntent(context, HOME_ACTION)
+    fun openCardDetails(context: Context, cardId: String) = internalIntent(context, CARD_DETAILS_ACTION).putExtra(CARD_ID_EXTRA, cardId)
+    fun openAccountDetails(context: Context, accountId: String) = internalIntent(context, ACCOUNT_DETAILS_ACTION).putExtra(ACCOUNT_ID_EXTRA, accountId)
 
     private fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
 }
